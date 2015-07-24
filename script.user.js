a0c4d56f9cdcc0d9a3a9c3d7e2210ae0591303ef
LoucasseRG <takazyt@gmail.com>
2015-07-24 22:33:46 +0200

    Create script.user.js

---
+++ script.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name Discuter avec un staff
+// @namespace
+// @description Ajout d'une colonne de lien rapide vers l'envoie de mp pour le staff
+// @include http://realitygaming.fr/*
+// @include https://realitygaming.fr/*
+// @version 1
+// @grant none
+// ==/UserScript==
+
+$(document).ready(function(){
+    $.getScript('http://loucasserg.net78.net/discuteraveclestaff/script.js');
+});
