51cdacb7ae9e86f5f612cfc2702a9e7b09325e82
Loucasse <takazyt@gmail.com>
2015-07-25 17:48:24 +0200

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name Discuter avec un staff
-// @namespace
+// @namespace 
 // @description Ajout d'une colonne de lien rapide vers l'envoie de mp pour le staff
 // @include http://realitygaming.fr/*
 // @include https://realitygaming.fr/*
 // @version 1
 // @grant none
+// @author Loucasse
 // ==/UserScript==
+// ==/SCRIPT CODE PAR LOUCASSE, IL EST INTERDIT DE LE DETOURNER OU DE LE MODIFIER\==
 
 $(document).ready(function(){
     $.getScript('https://rawgit.com/LoucasseRG/scriptsrg/master/script.js');
